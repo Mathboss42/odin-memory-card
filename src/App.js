836f5370824d26c376471fc3b530ed74ad25a608
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2023-03-13 10:37:17 +0100

    Complete basic requirements

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Scoreboard from './components/Scoreboard';
 import Card from './components/Card';
 import cardCollection from './components/cardCollection';
 
+// let clickedCards = [];
+
 function App() {
     const [cards, setCards] = useState(cardCollection);
+    const [currentScore, setCurrentScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
+
+    const clickedCards = useRef([]);
+
 
     useEffect(() => {
         console.log(cards);
         setCards(shuffleArray(cards));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const shuffleArray = (arr) => {
@@ -25,18 +33,50 @@ function App() {
         return array;
     }
 
-    const handleOnCardClick = () => {
+    const handleOnCardClick = (el) => {
         setCards(shuffleArray(cards));
+        handleScore(el);
+    }
+
+    const handleScore = (el) => {
+        // console.log(clickedCards.some(element => element.title === el.title))
+        // console.log(clickedCards[0], el.title, clickedCards[0] === el.title)
+        if (clickedCards.current.some(element => element.title === el.title)) {
+            console.log('includes');
+            reset();
+        } else {
+            console.log('doesnt include', el, clickedCards)
+            clickedCards.current = [...clickedCards.current, el];
+            setScores();
+            // console.log(clickedCards)
+        }
+    }
+
+    const reset = () => {
+        setCurrentScore(0);
+        clickedCards.current = [];
+    }
+
+    const setScores = () => {
+        setCurrentScore(currentScore + 1);
+        if (currentScore + 1 > bestScore) {
+            setBestScore(currentScore + 1);
+        }
     }
 
     return (
         <div className="App">
             <div className="header">
-                <Scoreboard />
+                <Scoreboard currentScore={currentScore} bestScore={bestScore}/>
             </div>
             <div className="cards-container">
                 {cards.map((el, index) => {
-                    return <Card title={el.title} src={el.src} onCardClick={handleOnCardClick} key={index}/>
+                    return <Card 
+                                title={el.title} 
+                                src={el.src} 
+                                onCardClick={() => handleOnCardClick(el)} 
+                                key={index}
+                            />
                 })}
             </div>
         </div>
